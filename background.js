1c283ffc7dada33fa7440a0e15a169be3d443be8
Nantha <nanthu1593@gmail.com>
2023-12-27 09:01:32 +0800

    event listener added
    event listener added

--- background.js
+++ background.js
@@ -1,1 +1,61 @@
-console.log("haii from bg")
+console.log("haii from bg");
+
+chrome.runtime.onMessage.addListener((data) => {
+  const { event, values } = data;
+
+  switch(event){
+    case 'onStart':
+        handleStart(values);
+        break;
+    case 'onStop':
+        handleStop();
+        break;
+    default:
+        break;
+  }
+});
+
+
+async function handleStop(){
+    stopAlarm();
+}
+
+async function handleStart(reloadInterval) {
+    console.log("hai", reloadInterval.timerValueMS);
+
+  reloadInterval = parseInt(reloadInterval.timerValueMS);
+
+  if (reloadInterval && typeof reloadInterval === "number") {
+    reloadInterval = reloadInterval * 1000;
+    console.log("hai", reloadInterval);
+     createAlarm();
+    // setInterval(() => {
+    //   chrome.tabs.query(
+    //     { active: true, currentWindow: true },
+    //     async function (tab) {
+    //       console.log("hai", tab);
+
+    //       chrome.tabs.reload(tab[0].id);
+    //     }
+    //   );
+    // }, reloadInterval);
+  }
+}
+
+
+
+const ALARM_JOB_REFRESHER = "CREATE_TIMER";
+async function createAlarm() {
+    console.log("Calling create alarms....")
+  chrome.alarms.create(ALARM_JOB_REFRESHER, { periodInMinutes: 1.0 });
+}
+
+async function stopAlarm() {
+    console.log("Calling Stop alarms....")
+
+  chrome.alarms.clearAll();
+}
+
+chrome.alarms.onAlarm.addListener(() => {
+  console.log("Alarm Job running");
+});
--- script.js
+++ script.js
@@ -1,50 +1,19 @@
-let reloadInterval = 0;
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("setupReload").addEventListener("click", setupReload);
-  document.getElementById("stopApp").addEventListener("click", stopAlarm);
-});
 
 
-async function setupReload() {
-  reloadInterval = document.getElementById("refresh_timer").value;
-  console.log(typeof reloadInterval);
-  reloadInterval = parseInt(reloadInterval);
-  console.log(typeof reloadInterval);
+const reloadStart = document.getElementById("setupReload");
+const timerValue = document.getElementById("refresh_timer");
+const stopTimer = document.getElementById("stopApp");
 
-  if (reloadInterval && typeof reloadInterval === "number") {
-    reloadInterval = reloadInterval * 1000;
-    console.log("hai", reloadInterval);
-    document.getElementById("deathNew").innerHTML = "Done";
-    setInterval(() => {
-      chrome.tabs.query(
-        { active: true, currentWindow: true },
-        async function (tab) {
-          console.log("hai", tab);
 
-          chrome.tabs.reload(tab[0].id);
-        }
-      );
-    }, reloadInterval);
+reloadStart.onclick = ()=>{
+  
+ 
+  const values = {
+    timerValueMS :timerValue.value
   }
+  chrome.runtime.sendMessage({event:"onStart",values});
 }
 
-const ALARM_JOB_REFRESHER = "CREATE_TIMER";
-async function createAlarm() {
-  chrome.alarms.create(
-    ALARM_JOB_REFRESHER,
-
-    { reload_interval: 1000 },
-    () => {}
-  );
+stopTimer.onclick=()=>{ 
+  chrome.runtime.sendMessage({event:"onStop"}); 
 }
-
-async function stopAlarm() {
-    chrome.alarms.clearAll(  )
-  }
-
-
-
-chrome.alarms.onAlarm.addListener(() => {
-  console.log("Alarm Job running");
-});
